902e15367e0fee6d96a9ef850e0cf5d711541cca
tonnipaul <ariyoadeoyepaul@gmail.com>
2025-11-28 03:28:08 +0100

    chore: install sharp add wild crad image config to next

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production' ? true : false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
