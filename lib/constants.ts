3a0e53c087903353ec8ff088f1cbddfdd29c95ac
Inioluwa <inisoetan2014@gmail.com>
2025-12-17 11:06:25 +0100

    refactor(constants.ts): update date time constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const DATE_FORMAT_DMY = 'DD MMM, YYYY';
 export const TIME_FORMAT_HM = 'HH:mm';
-export const DATE_FORMAT_MON_DAY = 'MMM DD';
+export const DATE_FORMAT_MD = 'MMM DD';
+export const TIME_FORMAT_AM_PM = 'h:mm A';
+export const DAY_MONTH_FORMAT = 'DD MMM';
